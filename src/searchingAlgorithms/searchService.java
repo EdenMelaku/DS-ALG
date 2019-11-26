a2081792050c7e480b36bfb0db27f7e642322e1e
eden melaku <edenzzzmelaku@gmail.com>
2019-11-26 17:53:45 +0300

    searching algorithms are fixed

--- src/searchingAlgorithms/searchService.java
+++ src/searchingAlgorithms/searchService.java
@@ -10,5 +10,56 @@ public class searchService {
      return -1;
     }
 
+    public static int BinarySerarch1(int[]numbers, int key){
+        int lb=0,ub=numbers.length-1,mid=0;
+        boolean increasing=true;
+
+
+        if(numbers[0]>numbers[1])  increasing=false;
+
+        while(lb<=ub){
+            mid=(lb+ub)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            else if(key<numbers[mid]){
+                if(increasing)  ub=mid-1;
+                else lb=mid+1;
+
+            }
+            else{
+                if(increasing) lb=mid+1;
+                else ub=mid+1;
+            }
+        }
+        return (-1-mid);
+    }
+
+    public static int BinarySerarch2(int[]numbers, int key, int lb,int ub){
+        int mid=(lb+ub)/2;
+        boolean increasing=true;
+
+
+        if(numbers[0]>numbers[1])  increasing=false;
+
+        if (lb<=ub){
+            mid=(lb+ub)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            else if(key<numbers[mid]){
+                if(increasing)  ub=mid-1;
+                else lb=mid+1;
+
+
+            }
+            else{
+                if(increasing) lb=mid+1;
+                else ub=mid+1;
+            }
+        }
+        return (-1-mid);
+    }
+
 
 }
