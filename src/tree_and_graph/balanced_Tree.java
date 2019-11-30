99ff00bd6c1f05ff56d4f8ecc35324231b890046
eden melaku <edenzzzmelaku@gmail.com>
2019-11-30 17:00:41 +0300

    methods are implemented but not tested yet

---
+++ src/tree_and_graph/balanced_Tree.java
@@ -0,0 +1,31 @@
+package tree_and_graph;
+/*
+Implement a function to check if a tree is balanced. For the purposes of this question,
+a balanced tree is defined to be a tree such that no two leaf nodes differ in distance
+from the root by more than one.
+ */
+import java.util.ArrayList;
+
+public class balanced_Tree extends Node {
+    Node node;
+    public balanced_Tree(ArrayList<Node> children) {
+        super(children);
+    }
+    int min_depth=0;
+    int max_depth=0;
+    public void find_depth(int depth, Node node){
+        if(node.children!=null){
+            depth++;
+            for(int i=0;i<node.children.size();i++){
+                find_depth(depth,node.children.get(i));
+            }
+        }
+        if(depth<min_depth) min_depth=depth;
+        if(depth>max_depth) max_depth=depth;
+    }
+    public boolean is_balanced(){
+     find_depth(0,node);
+     return (max_depth-min_depth)<=1;
+    }
+    
+}
