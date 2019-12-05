92d226a580148e329a6d876b138d97c1c07dd460
eden melaku <edenzzzmelaku@gmail.com>
2019-12-05 11:45:57 +0300

    errors are fixed

--- src/tree_and_graph/BST.java
+++ src/tree_and_graph/BST.java
@@ -1,4 +1,29 @@
 package tree_and_graph;
-
+/*
+Given a sorted (increasing order) array, write an algorithm to create a binary tree with
+minimal height.
+ */
 public class BST {
+    class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+    }
+
+    Node createBST(int[] numbers, Node root, int low, int high) {
+        int mid = low + (high - low / 2);
+        root.data = numbers[mid];
+        if (low < high) {
+            root.left = createBST(numbers, root, low, mid - 1);
+            root.right = createBST(numbers, root, mid + 1, high);
+        }
+        return root;
+    }
+
+
 }
--- src/tree_and_graph/CheckPath.java
+++ src/tree_and_graph/CheckPath.java
@@ -1,4 +1,22 @@
 package tree_and_graph;
 
+import java.util.LinkedList;
+
+/*
+Given a directed graph, design an algorithm to find out whether there is a route be-
+tween two nodes.
+ */
 public class CheckPath {
+
+    Node node1;
+    Node node2;
+
+    enum  state{
+     unvisited, visited,visiting;
+    }
+    public static boolean search(Graph g, Node start, Node end){
+        LinkedList<Node> list=new LinkedList<>();//operates as stack
+
+        for(Node )
+    }
 }
