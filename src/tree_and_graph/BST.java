1b05b285e0d4fc0fdc90a999998aedd4846d560c
eden melaku <edenzzzmelaku@gmail.com>
2019-12-07 11:48:15 +0300

    first common ancestor algorithm

--- src/tree_and_graph/BST.java
+++ src/tree_and_graph/BST.java
@@ -56,7 +56,28 @@ nodes at each depth (eg, if you have a tree with depth D, you’ll have D linked
         return out;
 
     }
-    
+
+    /*
+    Design an algorithm and write code to find the first common ancestor of two nodes
+in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+necessarily a binary search tree.
+     */
+
+    public Node commonAncestor(Node root, Node p, Node q) {
+        if (covers(root.left, p) && covers(root.left, q))
+            return commonAncestor(root.left, p, q);
+        if (covers(root.right, p) && covers(root.right, q))
+            return commonAncestor(root.right, p, q);
+        return root;
+         }
+  private boolean covers(Node root, Node p) { /* is p a child of root? */
+         if (root == null) return false;
+         if (root == p) return true;
+         return covers(root.left, p) || covers(root.right, p);
+         }
+
+       
+
 
 
 }
