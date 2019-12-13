1b0255d0793f1db2c4652f00c5ebc1d720b9bf4d
eden melaku <edenzzzmelaku@gmail.com>
2019-12-13 20:18:34 +0300

    find pair sum

---
+++ src/exercies_on_crack_the_code/PairSum.java
@@ -0,0 +1,8 @@
+package exercies_on_crack_the_code;
+/*
+given an array A[] of n numbers and another number x, determines whether or not there exist two elements in S whose
+sum is exactly x.
+ */
+public class PairSum {
+
+}
