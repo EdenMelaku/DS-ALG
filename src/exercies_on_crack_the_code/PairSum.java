0972059aa804e6cf7dbf89bd9b47ba21ef696537
eden melaku <edenzzzmelaku@gmail.com>
2019-12-13 20:50:11 +0300

    unit test

--- src/exercies_on_crack_the_code/PairSum.java
+++ src/exercies_on_crack_the_code/PairSum.java
@@ -5,4 +5,21 @@ sum is exactly x.
  */
 public class PairSum {
 
+    public static boolean findSum(int[] A,int sum){
+        boolean[] value=new boolean[1000];
+        for(int i=0;i<A.length;i++) {
+            int dif = sum - A[i];
+            if (dif >= 0 && value[dif]) {
+                return true;
+            }
+
+            value[A[i]] = true;
+        }
+            return false;
+    }
+
+    public static void main(String ... args) {
+        int[]A={1,5,6,-9,4,2};
+        System.out.println(findSum(A,31));
+    }
 }
