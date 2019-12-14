74f5ab38165997ebe6b8141610332fcadeda58fd
eden melaku <edenzzzmelaku@gmail.com>
2019-12-14 16:32:13 +0300

    find odd occurance

--- src/exercies_on_crack_the_code/OddOccurance.java
+++ src/exercies_on_crack_the_code/OddOccurance.java
@@ -1,4 +1,21 @@
 package exercies_on_crack_the_code;
-
+/*
+Given an array of positive integers. All numbers occur even number of times except one number which occurs odd number of times. Find the
+number in O(n) time & constant space.
+ */
 public class OddOccurance {
+
+    public static int getOddOcurrance(int[]A) {
+        int res=A[0];
+
+        for(int i=0;i<A.length;i++) {
+            res=res^A[i];
+        }
+        return res;
+    }
+
+    public static void main(String ... args) {
+        int[]A={1,4,5,7,2,4,5,6,2,1,1,4,1};
+        System.out.println(getOddOcurrance(A));
+    }
 }
