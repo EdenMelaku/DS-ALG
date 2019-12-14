9c58de2c8c808bb631a0c5ccee0d5aa7f645ab81
eden melaku <edenzzzmelaku@gmail.com>
2019-12-14 14:17:30 +0300

    fixed some errors still unfinisnhed

--- src/exercies_on_crack_the_code/MajorityElement.java
+++ src/exercies_on_crack_the_code/MajorityElement.java
@@ -1,4 +1,20 @@
 package exercies_on_crack_the_code;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+/*
+Majority Element: A majority element in an array A[] of size n is an element that appears more than n/2 times (and hence there is at most one
+such element).
+ */
 public class MajorityElement {
+   
+    public static int getMajorityElement(int[]A){
+        int majority=0;
+       int[]B=new int[1000];
+        for(int i=0;i<A.length;i++) {
+        }
+return 1;
+    }
+
 }
